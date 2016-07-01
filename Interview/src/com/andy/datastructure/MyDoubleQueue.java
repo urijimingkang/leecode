31a7f3621acd57d0b15317c6bde11330a2942021
aweng <jimingkang@gmail.com>
2016-06-30 18:23:11 -0700

    data structure :add MyDoubleEndQueue class

---
+++ Interview/src/com/andy/datastructure/MyDoubleQueue.java
@@ -0,0 +1,61 @@
+package com.andy.datastructure;
+
+/**
+ * Created by aweng on 6/30/2016.
+ */
+public class MyDoubleQueue<T> {
+    private LinkNode<T>  head;
+    private LinkNode<T> last;
+    private int size;
+    public MyDoubleQueue(){
+        head=new LinkNode<T>(null,null);
+        last=head;
+    }
+
+    public void offer(T data){//添加到队尾部
+        LinkNode<T> tmp=new LinkNode<T>(data);
+        last.next=tmp;
+        last=tmp;
+        size++;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public  T peek(){
+        return size==0?null:head.next.data;
+    }
+    public T poll(){//从头取元素
+        if(isEmpty()){
+            return null;
+        }
+        else
+        {LinkNode<T> tmp=head.next;
+            head.next =tmp.next;
+            size--;
+            if(size==0)
+            {
+                last=head;
+            }
+
+            return tmp.data;
+        }
+
+    }
+    public static void main(String[]as){
+        MyDoubleQueue<Integer> myQueue=new MyDoubleQueue<Integer>();
+
+        for(int i=0;i<10;i++)
+        {myQueue.offer(i);
+
+        }
+        while(!myQueue.isEmpty())
+        System.out.print(myQueue.poll());
+        System.out.println();
+    }
+
+
+
+
+
+
+}
