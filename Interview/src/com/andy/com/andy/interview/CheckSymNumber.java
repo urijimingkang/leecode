324db5a048ed417d75bee994180077bb01c53e66
aweng <jimingkang@gmail.com>
2016-07-06 17:10:52 -0700

    尝试用递归方式求回文数字，没成功

--- Interview/src/com/andy/com/andy/interview/CheckSymNumber.java
+++ Interview/src/com/andy/com/andy/interview/CheckSymNumber.java
@@ -11,6 +11,7 @@ public class CheckSymNumber {
         System.out.println(isSymNumber(a));
 
     }
+    //判断一个数是否回文
     public static boolean isSymNumber(int a) {
         int length = getLenOfNumber(a);
         int len = length;
@@ -25,17 +26,39 @@ public class CheckSymNumber {
 
 
             System.out.println("b=" + b + " c=" + c + " len=" + len);
-            System.out.println("(a%power(len-i))" + (a % power(len - i)));
-            a = a % power(len - 1);
-            System.out.println("(a/power(i))" + (a / power(i)));
-            a = a / power(1);
+            a = a % power(len - 1);  //去掉头一个数
+            a = a / power(1);         //去掉尾部一个数
             len = getLenOfNumber(a);
             if (b != c) {
                 flag = false;
                 break;
 
             }
-            
+
+        }
+        return flag;
+    }
+
+    //判断一个数是否回文，递归方式，没解决，有时间再做
+    public static boolean isSymNumber2(int a) {
+        int length = getLenOfNumber(a);
+        int len = length;
+        System.out.println(len);
+
+        int b = 0, c = 0;
+
+        boolean flag = true;
+        if (a > 0) {
+
+            b = a / power(len - 1);
+            c = a % power(1);
+
+            a = a % power(len - 1);  //去掉头一个数
+            a = a / power(1);         //去掉尾部一个数
+            len = getLenOfNumber(a);
+
+
+            flag = isSymNumber2(a);
         }
         return flag;
     }
