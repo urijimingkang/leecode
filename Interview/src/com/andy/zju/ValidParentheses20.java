0efeeeb8b37d5cf6a2ce499238ef3e3509a7ce9a
urijimingkang <jiming_kang@my.uri.edu>
2021-08-04 14:36:07 -0600

    test workspace.xml

--- Interview/src/com/andy/zju/ValidParentheses20.java
+++ Interview/src/com/andy/zju/ValidParentheses20.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class ValidParentheses20 {
     public static void main(String[] args) {
+
         //
         System.out.println(isValid(")("));
         System.out.println(isValid("()"));
