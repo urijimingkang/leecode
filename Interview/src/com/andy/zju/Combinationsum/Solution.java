7891aeaf4fc850273c648b6d67e78996418cfc12
urijimingkang <88399395+urijimingkang@users.noreply.github.com>
2021-08-04 14:42:57 -0400

    combinationsum

---
+++ Interview/src/com/andy/zju/Combinationsum/Solution.java
@@ -0,0 +1,32 @@
+package com.andy.zju.Combinationsum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, target, result, path, 0);
+        return result;
+    }
+    
+    public void dfs(int[] candidates, int target, List<List<Integer>> result, List<Integer> path, int index){
+        if(target < 0) return; 
+        
+        if(target == 0){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = index; i < candidates.length && candidates[i] <= target; i++){
+            if(i > index && candidates[i] == candidates[i - 1]) continue;
+            // i > index很关键， 确保i为起点的时候不需要跟前面的数字check
+            // in other words, 我们已经到了下一层不用跳过
+            path.add(candidates[i]);
+            dfs(candidates, target - candidates[i], result, path,i + 1);
+            path.remove(path.size()-1);
+        }
+    }
+}
