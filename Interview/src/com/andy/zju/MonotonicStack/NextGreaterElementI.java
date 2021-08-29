2e237da51f4345c3cfa3e39d9e29667c4ee4cb84
urijimingkang <jiefangli0605@gmail.com>
2021-08-29 19:26:38 -0400

    monotonic stack

---
+++ Interview/src/com/andy/zju/MonotonicStack/NextGreaterElementI.java
@@ -0,0 +1,31 @@
+package com.andy.zju.MonotonicStack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] res = new int[nums1.length];
+
+        for(int i = 0; i < nums2.length; i++) {
+            while(!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+
+        while(!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+
+        for(int j = 0; j < nums1.length; j++) {
+            res[j] = map.get(nums1[j]);
+        }
+        return res;
+
+
+    }
+}
