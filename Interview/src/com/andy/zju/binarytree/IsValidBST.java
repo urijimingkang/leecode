ba113ee2c423e1aaf406882733590cbbfa0252b5
urijimingkang <jiming_kang@my.uri.edu>
2021-10-17 21:35:29 -0600

    isvalidbst lowerstcommonAncestor AllNodeDistKBinTree WidthofBintree

---
+++ Interview/src/com/andy/zju/binarytree/IsValidBST.java
@@ -0,0 +1,26 @@
+package com.andy.zju.binarytree;
+
+import com.andy.ds.TreeNode;
+
+import java.util.Stack;
+
+class IsValidBST {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        Integer prev = null; 
+        
+        while(!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root); 
+                root = root.left;
+            }
+            root = stack.pop(); 
+            if(prev != null && root.val <= prev) {
+                return false; 
+            }
+            prev = root.val; 
+            root = root.right; 
+        }
+        return true; 
+    }
+}
---
+++ Interview/src/com/andy/zju/binarytree/LowestCommonAncestor.java
@@ -0,0 +1,40 @@
+package com.andy.zju.binarytree;
+
+import com.andy.ds.TreeNode;
+
+class LowestCommonAncestor {
+    class Result {
+        public TreeNode ans;
+        public int count; 
+        Result(TreeNode a, int c) {
+            ans = a; count = c; 
+        }
+    }
+    
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+        return helper(root, p, q).ans; 
+    }
+    
+    Result helper(TreeNode root, int p, int q) {
+        if(root == null) {
+            return new Result(null, 0); 
+        }
+        
+        Result left = helper(root.left, p, q); 
+        Result right = helper(root.right, p, q); 
+        
+        if(left.ans != null) {
+            return new Result(left.ans, 2); 
+        }
+        if(right.ans != null) {
+            return new Result(right.ans, 2); 
+        }
+        
+        int count = root.val == p || root.val == q ? 1 : 0;
+        
+        count += left.count + right.count; 
+        
+        return new Result(count == 2 ? root:null, count);
+    }
+}
